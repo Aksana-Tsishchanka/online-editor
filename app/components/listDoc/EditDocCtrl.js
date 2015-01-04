76f94623f6e6734dcba7ddbb8617b9c39c2db851
Aksana_Tsishchanka <oks1-92@mail.ru>
2015-01-04 19:48:28 +0300

    remove unused file

--- app/components/listDoc/EditDocCtrl.js
+++
@@ -1,8 +0,0 @@
-editDocApp.controller('EditDocCtrl', function($scope, DocsService, $stateParams){
-    if (typeof ($stateParams.id) != "undefined") {
-        var id = $stateParams.id;
-        $scope.doc = DocsService.getDoc(id);
-    }
-    
-    $scope.saveEditDoc = DocsService.save($scope.doc);
-}); 
