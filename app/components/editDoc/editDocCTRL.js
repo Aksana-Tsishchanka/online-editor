0cb9de855dd0bd44158d1a2a2fe0087cff9ccf3f
Aksana-Tsishchanka <oks1-92@mail.ru>
2015-01-26 11:15:32 +0300

    Goto view for buttom Save

--- app/components/editDoc/editDocCTRL.js
+++ app/components/editDoc/editDocCTRL.js
@@ -9,27 +9,18 @@ editDocApp.controller('EditDocCtrl', function($scope, $rootScope, DocsService, $
     }
     
     $scope.saveEditDoc = function() {
-    		$scope.$watch('doc.description', function(v) {
-				// so long as we are not in the
-				// $compile phase
-				if (v) {
-				// Render the htmlBody as trusted HTML
+
+    	$scope.$watch('doc.description', function(v) {
+		// so long as we are not in the
+		// $compile phase
+			if (v) {
+				// Render the descriptionHtmlSafe as trusted HTML
 				$scope.doc.descriptionHtmlSafe =
-					$sce.trustAsHtml($scope.doc.description);
+				$sce.trustAsHtml($scope.doc.description);			
+			}
+		});
 
-				$scope.doc.description = $scope.doc.descriptionHtmlSafe;					
-				}
-
-
-			});
-
-    		DocsService.save($scope.doc);
-
-    }
-
-
-    
+    	DocsService.save($scope.doc);
+    }	
 		
-		
-	
 }); 
